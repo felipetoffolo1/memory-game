bb9ff6fb3135d4dc86dbd1dcf0fe9f2ab5d9bf0b
felipe.toffolo <felipe.toffolo@gmail.com>
2017-12-08 12:39:04 -0200

    Congratulations screen, code comments, new colors

--- js/app.js
+++ js/app.js
@@ -1,15 +1,20 @@
+
 var shuffleArray = function (array) {
   for (let i = array.length - 1; i > 0; i--) {
     let j = Math.floor(Math.random() * (i + 1));
     [array[i], array[j]] = [array[j], array[i]];
   }
 };
+// Format number to 2 digits
+var formatNumber = function (myNumber) {
+  return ('0' + myNumber).slice(-2);
+};
 
 var options = ['fa-superpowers', 'fa-thermometer-quarter', 'fa-bed', 'fa-cab', 'fa-bell-o', 'fa-flash', 'fa-flask', 'fa-gift'];
+// var options = ['fa-superpowers', 'fa-thermometer-quarter'];
 
 var cardsToBuild = options.slice();
 cardsToBuild = cardsToBuild.concat(options.slice());
-shuffleArray(cardsToBuild);
 var cardsBuilded = {};
 
 var response = [];
@@ -18,31 +23,52 @@ var moves = 0;
 var startDate = false;
 var stars = 3;
 var rightAnswerNumber = 0;
+var countDown;
 
+// Init or restart the game
 var init = function () {
+  // shuffle the cards
+  shuffleArray(cardsToBuild);
+  // Reset variables
+  cardsBuilded = {};
+  moves = 0;
+  startDate = false;
+  stars = 3;
+  rightAnswerNumber = 0;
+  response = [];
+  // Clean container
+  document.getElementById('container').innerHTML = '';
+  // construct ao the cards, create and add to a object
   for (var index in cardsToBuild) {
     var newCard = new Card(cardsToBuild[index]);
     newCard.create();
     cardsBuilded[newCard.id] = newCard;
   }
   updateStars();
+  // Show the right elements on screen
+  document.getElementById('container').style = 'display:flex';
+  document.getElementById('result').style = 'display:none';
 };
 
+// Function to start the timer
 var startTimer = function () {
   if (!startDate) {
     startDate = new Date().getTime();
-    var countDown = setInterval(function () {
+    countDown = setInterval(function () {
       var now = new Date().getTime();
       var timeDiference = now - startDate;
-      var minutes = Math.floor((timeDiference % (1000 * 60 * 60)) / (1000 * 60));
-      var seconds = Math.floor((timeDiference % (1000 * 60)) / 1000);
-      document.getElementById('timer').innerHTML = minutes + 'm ' + seconds + 's ';
+      var minutes = formatNumber(Math.floor((timeDiference % (1000 * 60 * 60)) / (1000 * 60)));
+      var seconds = formatNumber(Math.floor((timeDiference % (1000 * 60)) / 1000));
+      document.getElementById('timer').innerHTML = minutes + ':' + seconds;
     }, 1000);
   }
 };
+
+// Function called everytime move is made
 var newMove = function () {
   moves++;
   document.getElementById('moves').innerHTML = moves + ' moves';
+  // Alter the number of star according to moves number
   switch (moves) {
     case 12:
       stars = 2;
@@ -58,6 +84,8 @@ var newMove = function () {
   }
   updateStars();
 };
+
+// Set the stars on the UI
 var updateStars = function () {
   var starContainer = document.getElementById('stars');
   starContainer.innerHTML = '';
@@ -68,30 +96,47 @@ var updateStars = function () {
   }
   for (i = 0; i < (3 - stars); i++) {
     var staroIcon = document.createElement('I');
-    staroIcon.classList.add('fa', 'fa-2x', 'star','fa-star-o');
+    staroIcon.classList.add('fa', 'fa-2x', 'star', 'fa-star-o');
     starContainer.appendChild(staroIcon);
   }
 };
+
+// Function called when a right answer is given
 var rightAnswer = function () {
+  // Add the number of right answers
   rightAnswerNumber++;
+  // Call the correct function for the cards
   response[0].correct();
   response[1].correct();
+  // Clean responses
   response = [];
+  // Check if the number of correct Answers is equal the number of options
   if (rightAnswerNumber === options.length) {
-    alert('congrats');
+    // Show congrats msg
+    document.getElementById('container').style = 'display:none';
+    document.getElementById('result').style = 'display:block';
+    // Stop timer
+    clearInterval(countDown);
   }
 };
+
+// Function called when a wrong answer is given
 var wrongAnswer = function () {
   response[0].wrong();
   response[1].wrong();
   response = [];
 };
 
+// Card
+
+// Card constructor
 var Card = function (icon) {
   this.id = Math.random();
   this.icon = icon;
   this.turned = false;
 };
+
+// Function to createa new card on ui
 Card.prototype.create = function () {
   var container = document.getElementById('container');
   var cardItem = document.createElement('DIV');
@@ -101,25 +146,28 @@ Card.prototype.create = function () {
   var cardIcon = document.createElement('I');
   cardIcon.classList.add(this.icon, 'fa', 'fa-5x', 'card-icon');
   var backIcon = document.createElement('I');
-  backIcon.classList.add('fa-snowflake-o', 'fa', 'fa-5x', 'back-icon');
+  backIcon.classList.add('fa-gg', 'fa', 'fa-2x', 'back-icon');
   cardItem.appendChild(cardIcon);
   cardItem.appendChild(backIcon);
   container.appendChild(cardItem);
 };
-
+// Funciton when a card is wrong
 Card.prototype.wrong = function () {
   var cardItem = document.getElementById(this.id);
   cardItem.classList.add('wrong');
   this.turned = false;
   setTimeout(function () {
+    cardItem.classList.remove('wrong');
     cardItem.classList.remove('open');
   }, 1000);
 };
+// Function when a card is right
 Card.prototype.correct = function () {
   var cardItem = document.getElementById(this.id);
   cardItem.classList.add('right');
 };
 
+// Function when a card is turned
 Card.prototype.turn = function () {
   // Recover the selected card object
   var cardSelected = cardsBuilded[this.id];
@@ -133,8 +181,10 @@ Card.prototype.turn = function () {
     cardItem.classList.add('open');
     cardSelected.turned = true;
     if (response.length === 2) {
+      // Register the move
       newMove();
-      if (response[0].icon === response[1].icon && response[0].id !== response[1].id) {
+      // If the two options have the same icon
+      if (response[0].icon === response[1].icon) {
         rightAnswer();
       } else {
         wrongAnswer();
